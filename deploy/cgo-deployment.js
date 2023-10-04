bc1d94ed12e15c4ca8f9410938ab251aef2e21cc
Shadab Akhtar <akhtarshadab415@gmail.com>
2023-10-04 15:46:26 +0530

    cgold contract deployment

--- deploy/cgo-deployment.js
+++ deploy/cgo-deployment.js
@@ -3,16 +3,15 @@ module.exports = async ({ getNamedAccounts, deployments, network }) => {
   const { deploy } = deployments;
   const { owner } = await getNamedAccounts();
 
-  // const { address: tknAddress } = await deploy("Goldtoken", {
-  //   from: owner,
-  // });
-  // console.log("Goldtoken deployed to:", tknAddress);
+  const { address: tknAddress } = await deploy("Goldtoken", {
+    from: owner,
+  });
+  console.log("Goldtoken deployed to:", tknAddress);
 
   // deploy with contructor
   const { address: cgoController } = await deploy("CGOController", {
     from: owner,
-    // args: [tknAddress],
-    args: ["0x8f9920283470F52128bF11B0c14E798bE704fD15"],
+    args: [tknAddress],
   });
 
   console.log("CGOController deployed to:", cgoController);
